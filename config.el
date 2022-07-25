3e403c4750e49197528e8be9e5cf6241d78187a8
Jeronimo Garcia <garciaj.uk@gmail.com>
2022-07-25 15:10:51 +0100

    Adding HML config

--- config.el
+++ config.el
@@ -125,6 +125,8 @@
 (setq which-key-idle-delay 0.7)
 (setq which-key-idle-secondary-delay 0.7)
 
+(define-key evil-normal-state-map (kbd "H") 'evil-window-top)
+(define-key evil-normal-state-map (kbd "L") 'evil-window-bottom)
 (define-key evil-normal-state-map (kbd "<left>") 'ignore)
 (define-key evil-visual-state-map (kbd "<left>") 'ignore)
 (define-key evil-normal-state-map (kbd "<right>") 'ignore)
