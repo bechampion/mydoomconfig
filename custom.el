c88bfbb1910c0b3c7dec5705b8e71c849ad5a129
jgarcia <garciaj.uk@gmail.com>
2022-07-30 13:30:12 +0100

    more org for darkokai fixes

--- custom.el
+++ custom.el
@@ -65,6 +65,8 @@
  '(org-date ((t (:inherit default :foreground "#06d8ff"))))
  '(org-level-1 ((t (:inherit variable-pitch :extend t :foreground "#ffac4a" :weight normal :height 1.0))))
  '(org-level-2 ((t (:inherit variable-pitch :extend t :foreground "#63de5d" :height 1.0))))
+ '(org-level-3 ((t (:inherit variable-pitch :extend t :foreground "#06d8ff" :height 1.0))))
+ '(org-level-4 ((t (:inherit variable-pitch :extend t :foreground "#E6DB74" :height 1.0))))
  '(region ((t (:inherit nil :extend t :background "gray30")))))
 (put 'customize-group 'disabled nil)
 (put 'customize-face 'disabled nil)
