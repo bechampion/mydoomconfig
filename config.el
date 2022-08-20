c4a4d4cb7e4fccb6a3bec36657417136566d8fd9
jgarcia <garciaj.uk@gmail.com>
2022-08-20 13:42:25 +0100

    13 for desktop?

--- config.el
+++ config.el
@@ -67,7 +67,7 @@
         "l" 'dired-find-file ))
 
 
-(setq doom-font (font-spec :family "UbuntuMono Nerd Font Mono" :size 17.5 :weight 'normal)
+(setq doom-font (font-spec :family "UbuntuMono Nerd Font Mono" :size 13.5 :weight 'normal)
       doom-big-font (font-spec :family "UbuntuMono Nerd Font Mono" :size 24))
 (setq doom-theme 'darkokai)
 ;; (beacon-mode 0)
