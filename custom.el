428f40535252fddd351d2341b645accdcf20350d
Jeronimo Garcia <garciaj.uk@gmail.com>
2022-08-21 10:09:32 +0100

    Adding whaterver custom settings i have on my pc

--- custom.el
+++ custom.el
@@ -3,70 +3,13 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ansi-color-names-vector
-   ["#282c34" "#ff6c6b" "#98be65" "#ECBE7B" "#51afef" "#c678dd" "#46D9FF" "#bbc2cf"])
- '(compilation-message-face 'default)
- '(custom-safe-themes
-   '("835868dcd17131ba8b9619d14c67c127aa18b90a82438c8613586331129dda63" default))
- '(exwm-floating-border-color "#191b20")
- '(fci-rule-color "#5B6268")
- '(helm-candidate-number-limit 50)
- '(highlight-changes-colors '("#ff8eff" "#ab7eff"))
- '(highlight-tail-colors
-   ((("#333a38" "#99bb66" "green")
-     . 0)
-    (("#2b3d48" "#46D9FF" "brightcyan")
-     . 20)))
- '(idle-update-delay 1.0)
- '(jdee-db-active-breakpoint-face-colors (cons "#1B2229" "#51afef"))
- '(jdee-db-requested-breakpoint-face-colors (cons "#1B2229" "#98be65"))
- '(jdee-db-spec-breakpoint-face-colors (cons "#1B2229" "#3f444a"))
- '(magit-diff-use-overlays nil)
- '(objed-cursor-color "#ff6c6b")
  '(package-selected-packages
-   '(markdown-preview-eww rainbow-blocks vterm dirvish transpose-frame mixed-pitch ewal-doom-themes company-fuzzy))
- '(pdf-view-midnight-colors (cons "#bbc2cf" "#282c34"))
- '(pos-tip-background-color "#E6DB74")
- '(pos-tip-foreground-color "#242728")
- '(rustic-ansi-faces
-   ["#282c34" "#ff6c6b" "#98be65" "#ECBE7B" "#51afef" "#c678dd" "#46D9FF" "#bbc2cf"])
- '(treemacs-follow-mode t)
- '(treemacs-show-cursor t)
- '(vc-annotate-background "#282c34")
- '(vc-annotate-color-map
-   (list
-    (cons 20 "#98be65")
-    (cons 40 "#b4be6c")
-    (cons 60 "#d0be73")
-    (cons 80 "#ECBE7B")
-    (cons 100 "#e6ab6a")
-    (cons 120 "#e09859")
-    (cons 140 "#da8548")
-    (cons 160 "#d38079")
-    (cons 180 "#cc7cab")
-    (cons 200 "#c678dd")
-    (cons 220 "#d974b7")
-    (cons 240 "#ec7091")
-    (cons 260 "#ff6c6b")
-    (cons 280 "#cf6162")
-    (cons 300 "#9f585a")
-    (cons 320 "#6f4e52")
-    (cons 340 "#5B6268")
-    (cons 360 "#5B6268")))
- '(vc-annotate-very-old-color nil)
- '(weechat-color-list
-   (unspecified "#242728" "#323342" "#F70057" "#ff0066" "#86C30D" "#63de5d" "#BEB244" "#E6DB74" "#40CAE4" "#06d8ff" "#FF61FF" "#ff8eff" "#00b2ac" "#53f2dc" "#f8fbfc" "#ffffff")))
+   '(nyan-mode vterm transpose-frame spinner rainbow-blocks mixed-pitch markdown-preview-eww markdown-mode lv ivy f ewal-doom-themes dirvish company-fuzzy)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(org-agenda-date-today ((t (:background "#242728" :foreground "#06d8ff" :inverse-video t :box nil :overline t :weight light :height 0.8 :width normal))))
- '(org-date ((t (:inherit default :foreground "#06d8ff"))))
- '(org-level-1 ((t (:inherit variable-pitch :extend t :foreground "#ffac4a" :weight normal :height 1.0))))
- '(org-level-2 ((t (:inherit variable-pitch :extend t :foreground "#63de5d" :height 1.0))))
- '(org-level-3 ((t (:inherit variable-pitch :extend t :foreground "#06d8ff" :height 1.0))))
- '(org-level-4 ((t (:inherit variable-pitch :extend t :foreground "#E6DB74" :height 1.0))))
- '(region ((t (:inherit nil :extend t :background "gray30")))))
+ )
 (put 'customize-group 'disabled nil)
 (put 'customize-face 'disabled nil)
