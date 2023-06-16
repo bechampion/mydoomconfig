c7ed36231a51f72cc3994ce134b8ca872df5969a
Jeronimo Garcia <garciaj.uk@gmail.com>
2023-06-16 10:28:32 +0100

    Fixing font and going back to doom-one

--- config.el
+++ config.el
@@ -114,9 +114,9 @@
     "n" 'find-file
     "l" 'dired-find-file ))
 
-(setq doom-font (font-spec :family "UbuntuMono Nerd Font Mono" :size 18.0 :weight 'normal)
+(setq doom-font (font-spec :family "UbuntuMono Nerd Font Mono" :size 13.5 :weight 'normal)
       doom-big-font (font-spec :family "UbuntuMono Nerd Font Mono" :size 24))
-(setq doom-theme 'doom-dracula)
+(setq doom-theme 'doom-one)
 (beacon-mode 1)
 (setq beacon-color "#50fa7b")
 ;; (treemacs-icons-dired-mode 1)
